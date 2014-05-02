b22ec8624a9d35ca49a0fb42482f25d5c94e7520
codestation <codestation404@gmail.com>
2014-05-02 11:07:41 -0430

    Added version bump script. Bump version so the sources can be uploaded to ubuntu ppa.

---
+++ version_bump.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+[ $# -eq 0 ] && { echo "Usage: $0 <version>"; exit 1; }
+
+sed -i "s/AC_INIT(\[vitamtp\], \[.*\], \[codestation@gmail.com\]/AC_INIT(\[vitamtp\], \[$1\], \[codestation@gmail.com\]/" configure.ac
+
+echo "Don't forget to update the changelog"
